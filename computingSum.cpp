e12232633678c33b7c6f05dc9070b734bd114fb1
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-09-19 15:15:32 +0200

    Create computingSum.cpp

---
+++ computingSum.cpp
@@ -0,0 +1,13 @@
+sum = 0;
+#pragma omp parallel shared(n,a,sum) private(sum_local)
+ {
+ sum_local = 0;
+ #pragma omp for
+ for (i=0; i<n; i++)
+ sum_local += a[i]; // form per-thread local sum
+
+ #pragma omp critical
+ {
+ sum += sum_local; // form global sum
+ }
+ }
