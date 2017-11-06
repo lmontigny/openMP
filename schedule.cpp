61ed5c53a06ce919c13192e72b4481c65c65d04e
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-11-06 10:54:40 +0100

    Update schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -2,14 +2,19 @@
 Way in which parallel loop iterations are distributed among the threads
 Depends upon whether each loop iteration yields the same amount of work
 Schedule clause  schedule(type[, chunk_size])
-Static schedule - each thread is assigned a fixed number of chunks (default)
+
+-Static schedule - each thread is assigned a fixed number of chunks (default)
 Chunks are groups of iterations (in contiguous order) that are assigned to threads
-Dynamic schedule - each thread grabs "chunk" iterations until all iterations are done
-Faster threads are assigned more iterations
-Interleaved is when chunks are assigned to the processors in a round-robin manner
-Guided is a variant of dynamic scheduling where successive chunks get smaller
-Runtime is determined at runtime and set by a environment variable
+
+-Dynamic schedule - each thread grabs "chunk" iterations until all iterations are done
+Faster threads are assigned more iterations (at runtime decided)
+
+-Guided is a variant of dynamic scheduling where successive chunks get smaller
+
+-Runtime is determined at runtime and set by a environment variable
 export OMP_SCHEDULE="dynamic, 100"
+
+-auto (give compiler the permission to be flexible)
 */
 
 
